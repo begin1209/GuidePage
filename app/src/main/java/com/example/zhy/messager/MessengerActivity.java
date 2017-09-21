9da7d97803c4e124c72141ed61265808eb951971
begin1209 <begin1209@126.com>
2017-09-22 00:53:56 +0800

    新增messenger示例

---
+++ app/src/main/java/com/example/zhy/messager/MessengerActivity.java
@@ -0,0 +1,16 @@
+package com.example.zhy.messager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.zhy.guidepage.R;
+
+
+public class MessengerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messager);
+    }
+}
